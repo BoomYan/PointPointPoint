798cc9667cec1fc6b9bc314e793ba5b5b8112826
BoomYan <jiamingyan@hotmail.com>
2015-03-04 09:53:54 -0500

    first commit

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (2f, 0, transform.position.z) * Time.deltaTime);
+	}
+}
